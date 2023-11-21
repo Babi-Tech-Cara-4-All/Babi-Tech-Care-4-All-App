e960c048db21d8b26f9ecba1806693894c57b16a
Thiago E Matumoto <thiagoeijimatumoto@gmail.com>
2023-11-21 19:37:39 -0300

    :lipstick: grind banner and scroll styles

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -5,23 +5,21 @@ const HomePage = () => {
   return (
     <div className={styles.homePageContainer}>
       <div className={styles.bannerHomePage}>
-        <div>
-        <h2>
-          Inovação e tecnologia moldando <br /> o futuro da saúde: <br />
-          prevenção, automação e precisão.
-        </h2>
-        <p>
-          A saúde é vital para a qualidade de vida. <br /> Essa revolução é essencial
-          na sociedade atual, tornando o futuro da saúde uma prioridade urgente.
-        </p>
-        <Botao cor="laranja">
-          <p>Saiba Mais</p>
-        </Botao>
-        <Botao cor="laranja">
-          <p>Fale conosco</p>
-        </Botao>
+        <div className={styles.infoContainer}>
+          <h2>
+            Inovação e tecnologia moldando <br /> o futuro da saúde: <br />
+            prevenção, automação e precisão.
+          </h2>
+          <p>
+            A saúde é vital para a qualidade de vida. <br /> Essa revolução é
+            essencial na sociedade atual, tornando o futuro da saúde uma
+            prioridade urgente.
+          </p>
+          <div className={styles.buttonsContainer}>
+            <Botao cor="laranja">Saiba Mais</Botao>
+            <Botao cor="laranja">Fale conosco</Botao>
+          </div>
         </div>
-
       </div>
     </div>
   );
