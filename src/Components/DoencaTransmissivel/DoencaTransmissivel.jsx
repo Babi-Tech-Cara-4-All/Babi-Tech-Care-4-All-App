0251031ecd2d080ef563ba21dc2e841a00357a38
Thiago E Matumoto <thiagoeijimatumoto@gmail.com>
2023-11-23 03:29:32 -0300

    Add informations and prevents

--- src/Components/DoencaTransmissivel/DoencaTransmissivel.jsx
+++ src/Components/DoencaTransmissivel/DoencaTransmissivel.jsx
@@ -1,16 +1,23 @@
-import React from 'react'
-import styles from "./DoencaTransmissivel.module.scss"
+import React from 'react';
+import styles from "./DoencaTransmissivel.module.scss";
 
-const DoencaTransmissivel = ({nome,casos, mortes, cor}) => {
+const DoencaTransmissivel = ({ nome, casos, mortes, cor, children }) => {
   return (
-    <div className={styles.cardDoenca}>
-      <h1 className={cor === "azul" ? styles.titleAzul: styles.titleLaranja}>{nome}</h1>
-      <p className={styles.number}>{casos}</p>
-      <p className={styles.text}>De casos</p>
-      <p className={styles.number}>{mortes}</p>
-      <p className={styles.text}>Mortes</p>
+    <div className={children ? styles.cardTratamento: styles.cardDoenca}>
+      <h1 className={cor === "azul" ? styles.titleAzul : styles.titleLaranja}>{nome}</h1>
+      
+      {children ? (
+        children
+      ) : (
+        <>
+          <p className={styles.number}>{casos}</p>
+          <p className={styles.text}>De casos</p>
+          <p className={styles.number}>{mortes}</p>
+          <p className={styles.text}>Mortes</p>
+        </>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default DoencaTransmissivel
+export default DoencaTransmissivel;
--- src/Pages/DoençasTransmissiveis/DoencaTransmissiveisCopy.jsx
+++ src/Pages/DoençasTransmissiveis/DoencaTransmissiveisCopy.jsx
@@ -1,4 +1,7 @@
 import styles from "./DoencaTransmissiveisCopy.module.scss";
+import Reason from "../../Components/Reason/Reason";
+import DoencaTransmissivel from "../../Components/DoencaTransmissivel/DoencaTransmissivel";
+import Botao from "../../Components/Botao/Botao";
 import vacina from "../../assets/imgDoencasTransmissiveis/vacina.png";
 import mascara from "../../assets/imgDoencasTransmissiveis/mascara.png";
 import IconBacterias from "../../assets/imgDoencasTransmissiveis/IconBacterias.png";
@@ -6,11 +9,9 @@ import IconProtozoarios from "../../assets/imgDoencasTransmissiveis/IconProtozoa
 import IconMetazoarios from "../../assets/imgDoencasTransmissiveis/IconMetazoarios.png";
 import IconVirus from "../../assets/imgDoencasTransmissiveis/IconVirus.png";
 import IconFungos from "../../assets/imgDoencasTransmissiveis/IconFungos.png";
-import menina from "../../assets/imgDoencasTransmissiveis/menina.png";
+import mulherFalando from "../../assets/imgDoencasTransmissiveis/mulherFalando.png";
 import mapa from "../../assets/imgDoencasTransmissiveis/mapa.png";
 import grafico from "../../assets/imgDoencasTransmissiveis/grafico.png";
-import Reason from "../../Components/Reason/Reason";
-import DoencaTransmissivel from "../../Components/DoencaTransmissivel/DoencaTransmissivel";
 
 const DoencaTransmissiveisCopy = () => {
   return (
@@ -42,94 +43,198 @@ const DoencaTransmissiveisCopy = () => {
             </div>
           </section>
           <section className={styles.gridDoencas}>
-            <h2>Principais doenças transmissiveis e motalidades</h2>
+            <h2>Principais doenças transmissiveis e mortalidades</h2>
             <div className={styles.gridDoencasContainer}>
-              <DoencaTransmissivel cor="azul" nome="HIV" casos="37.7 M" mortes="680 K"/>
-              <DoencaTransmissivel cor="laranja" nome="Tuberculose" casos="5.8 M" mortes="1.3 M"/>
-              <DoencaTransmissivel cor="azul" nome="Malaria" casos="241 M" mortes="627 K"/>
-              <DoencaTransmissivel cor="laranja" nome="Hepatite B" casos="296 M" mortes="820 K"/>
-              <DoencaTransmissivel cor="azul" nome="DTNs" casos="1.7 B" mortes="750 K"/>
+              <DoencaTransmissivel
+                cor="azul"
+                nome="HIV"
+                casos="37.7 M"
+                mortes="680 K"
+              />
+              <DoencaTransmissivel
+                cor="laranja"
+                nome="Tuberculose"
+                casos="5.8 M"
+                mortes="1.3 M"
+              />
+              <DoencaTransmissivel
+                cor="azul"
+                nome="Malaria"
+                casos="241 M"
+                mortes="627 K"
+              />
+              <DoencaTransmissivel
+                cor="laranja"
+                nome="Hepatite B"
+                casos="296 M"
+                mortes="820 K"
+              />
+              <DoencaTransmissivel
+                cor="azul"
+                nome="DTNs"
+                casos="1.7 B"
+                mortes="750 K"
+              />
             </div>
           </section>
           <section className={styles.gridCausadores}>
             <h2>AGENTES CAUSADORES</h2>
             <div className={styles.gridCausadoresContainer}>
-              <Reason icone={IconBacterias} texto="Bacterias" />
-              <Reason icone={IconMetazoarios} texto="Metazoários" />
-              <Reason icone={IconVirus} texto="Vírus" />
-              <Reason icone={IconFungos} texto="Fungos" />
-              <Reason icone={IconProtozoarios} texto="Protozoários" />
+              <Reason
+                icone={IconBacterias}
+                texto="Bacterias"
+                link="https://mundoeducacao.uol.com.br/amp/doencas/doencas-por-bacterias.htm"
+              />
+              <Reason
+                icone={IconMetazoarios}
+                texto="Metazoários"
+                link="https://www.bio.fiocruz.br/index.php/br/esquistossomose-e-outras-doencas-parasitarias"
+              />
+              <Reason
+                icone={IconVirus}
+                texto="Vírus"
+                link="https://m.brasilescola.uol.com.br/amp/doencas/doencas-causadas-virus.htm"
+              />
+              <Reason
+                icone={IconFungos}
+                texto="Fungos"
+                link="https://www.tuasaude.com/doencas-causadas-por-fungos/"
+              />
+              <Reason
+                icone={IconProtozoarios}
+                texto="Protozoários"
+                link="https://midia.atp.usp.br/plc/plc0501/impressos/plc0501_06.pdf"
+              />
             </div>
           </section>
-          {/* <section className={styles.dataContainer}>
-          <div className={styles.dataImage}>
-            <h1>Taxa de Mortalidade Infantil - Brasil</h1>
-            <img src={dado1} alt="taxa de mortalidade"></img>
-            <p>
-              Taxa de Mortalidade Infantil (por mil Nascidos Vivos). Regiões,
-              1990 a 2019
-            </p>
+        </div>
+      </div>
+      <section className={styles.informationSection}>
+        <div className={styles.gridInformation}>
+          <div className={styles.bodyInformation}>
+            <div className={styles.informacoes}>
+              <img src={mulherFalando} alt="" />
+              <ul>
+                <li>
+                  Em muitos países, a realização de testes para HIV é
+                  substancialmente mais baixa entre populações empobrecidas,
+                  mal-educadas ou em áreas rurais.
+                </li>
+                <br />
+                <li>
+                  Jovens mulheres (15-24 anos) na Região Africana da OMS têm
+                  maior risco de infecção por HIV do que seus colegas
+                  masculinos.
+                </li>
+                <br />
+                <li>
+                  Globalmente, os homens são menos propensos a fazer o teste de
+                  HIV ou receber terapia antirretroviral.
+                </li>
+              </ul>
+              <Botao>
+                {" "}
+                <a
+                  href="https://iris.who.int/bitstream/handle/10665/356584/9789240051140-eng.pdf?sequence=1"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Saiba Mais
+                </a>
+              </Botao>
+            </div>
+            <div className={styles.maps}>
+              <img src={mapa} alt="dado2"></img>
+              <img src={grafico} alt="dado3"></img>
+            </div>
           </div>
-          <div className={styles.dataText}>
-            <p className={styles.dataText1}>
-              A mortalidade infantil é um importante indicador de saúde e
-              condições de vida de uma população.
-            </p>
-            <p className={styles.dataText2}>
-              No Brasil, vem-se observando um declínio na taxa de mortalidade
-              nesse grupo.
-            </p>
+        </div>
+      </section>
+
+      <section className={styles.section2}>
+        <section className={styles.gridArea2}>
+          <div className={styles.bodyPage2}>
+            <section className={styles.gridPrevencoes}>
+              <h2>Prevenções</h2>
+              <div className={styles.gridPrevencoesContainer}>
+                <DoencaTransmissivel cor="azul" nome="HIV">
+                  <ul>
+                    <li>Vacinação HPV;</li>
+                    <li>O uso da camisinha em todas as relações sexuais;</li>
+                    <li>Testagem para HIV.</li>
+                  </ul>
+                </DoencaTransmissivel>
+                <DoencaTransmissivel cor="laranja" nome="Tuberculose">
+                  <ul>
+                    <li>Vacinação BCG;</li>
+                    <li>
+                      O tratamento da Infecção Latente da Tuberculose (ILTB).
+                    </li>
+                  </ul>
+                </DoencaTransmissivel>
+                <DoencaTransmissivel cor="azul" nome="Malaria">
+                  <ul>
+                    <li>Uso de mosquiteiros;</li>
+                    <li>Roupas que protejam pernas e braços; </li>
+                    <li>Telas em portas e janelas; uso de repelentes;</li>
+                    <li>Uso de repelentes</li>
+                  </ul>
+                </DoencaTransmissivel>
+                <DoencaTransmissivel cor="laranja" nome="Hepatite B">
+                  <ul>
+                    <li>Vacinação;</li>
+                    <li>Usar camisinha em todas as relações sexuais;</li>
+                    <li>Não compartilhar objetos de uso pessoal.</li>
+                  </ul>
+                </DoencaTransmissivel>
+                <DoencaTransmissivel cor="azul" nome="DTNs">
+                  <ul>
+                    <li>Vacinação;</li>
+                    <li>Cuidado com hiegene pessoal; </li>
+                    <li>Evitar contato com trasmissores;</li>
+                    <li>
+                      Evitar deixar ambientes propicios para o transmissor.
+                    </li>
+                  </ul>
+                </DoencaTransmissivel>
+                <DoencaTransmissivel
+                  cor="laranja"
+                  nome="Transmissíveis por Água"
+                >
+                  <ul>
+                    <li>Beber água que tenha sido filtrada e/ou fervida;</li>
+                    <li>Higienizar alimentos com água potável; </li>
+                    <li>Não jogar lixo fora do lixo.</li>
+                  </ul>
+                </DoencaTransmissivel>
+              </div>
+            </section>
+            <section className={styles.solitarioInformation}>
+              <div>
+                <h3>
+                  O emprego de medidas de controle de infecção também faz parte
+                  das ações de prevenção da doença, tais como:
+                  <li>
+                    manter ambientes bem ventilados e com entrada de luz solar;
+                  </li>
+                  <li>
+                    proteger a boca com o antebraço ou com um lenço ao tossir e
+                    espirrar (higiene da tosse);
+                  </li>
+                  <li>evitar aglomerações.</li>
+                </h3>
+                <a
+                  href="https://www.gov.br/aids/pt-br/assuntos/tuberculose/prevencao"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Saiba Mais
+                </a>
+              </div>
+            </section>
           </div>
         </section>
-*/}
-        </div>
-      </div>
-      {/* <section className={styles.dataSection}>
-      <div className={styles.gridAreaData}>
-        <div className={styles.bodyData}>
-          <div className={styles.dataBox}>
-            <p>
-              Taxa de mortalidade infantil (menores de um ano de idade) - 2019
-              (para cada mil nascidos vivos)
-            </p>
-            <img src={dado2} alt="dado2"></img>
-          </div>
-          <div className={styles.dataBox}>
-            <p>
-              Taxa de mortalidade na infância (menores de um cinco de idade) -
-              2019 (para cada mil nascidos vivos)
-            </p>
-            <img src={dado3} alt="dado3"></img>
-          </div>
-        </div>
-      </div>
-    </section>
-
-    <section className={styles.flipCard}>
-      <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
-        <div className={styles.front}>
-          <img src={kids} alt="" />
-          <button onClick={(e) => flipCard(e)}>Click to flip</button>
-        </div>
-        <div className={styles.back}>
-          <p>
-            O Brasil vem avançando na redução da mortalidade infantil, mas
-            ainda é preciso um grande esforço para enfrentar as diferenças
-            regionais e alcançar patamares mais baixos.
-          </p>
-          <div className={styles.starSpace}>
-            <img src={star} alt="star" />
-            <img src={arrow} alt="arrow" />
-          </div>
-          <p>
-            A mobilização não somente de todas as esferas de governo, mas de
-            toda a sociedade e de cada cidadão é importante para consolidar
-            essa redução, num movimento em defesa da vida.
-          </p>
-          <button onClick={(e) => flipCard(e)}>Click to flip</button>
-        </div>
-      </ReactCardFlip>
-    </section> */}
+      </section>
     </div>
   );
 };
