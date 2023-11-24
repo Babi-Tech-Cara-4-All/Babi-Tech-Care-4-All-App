792b61d0b61418ac96d50f30cd732b66d98681d8
Thiago E Matumoto <thiagoeijimatumoto@gmail.com>
2023-11-24 19:11:47 -0300

    add cards

--- src/Pages/MelhoriaGeralSaude/MelhoriaGeralSaude.jsx
+++ src/Pages/MelhoriaGeralSaude/MelhoriaGeralSaude.jsx
@@ -15,8 +15,10 @@ import i6 from "../../assets/imgMelhoria/i6.png";
 import info2 from "../../assets/imgMelhoria/info2.png";
 
 const MelhoriaGeralSaude = () => {
-  const [textoVisivel, setTextoVisivel] = useState("");
-  const [tituloAtivo, setTituloAtivo] = useState("");
+  const [textoVisivel, setTextoVisivel] = useState(
+    "Reforçar a prevenção e o tratamento dos problemas decorrentes do uso de substâncias, incluindo o abuso de drogas entorpecentes e uso nocivo do álcool."
+  );
+  const [tituloAtivo, setTituloAtivo] = useState("ODS 3.5");
 
   const odsList = [
     {
@@ -59,7 +61,7 @@ const MelhoriaGeralSaude = () => {
     "https://grupoft.com.br/seguranca-ao-dirigir/",
     "https://viverbem.unimedbh.com.br/maternidade/planejamento-familiar/planejamento-familiar/",
     "https://vidasaudavel.einstein.br/dependencia-quimica/",
-  ]; 
+  ];
 
   const nextImage = () => {
     setCurrentImage((prev) => (prev + 1) % images.length);
@@ -68,9 +70,6 @@ const MelhoriaGeralSaude = () => {
   const prevImage = () => {
     setCurrentImage((prev) => (prev - 1 + images.length) % images.length);
   };
-
-  
-
 
   return (
     <div className={styles.nTransmissivelContainer}>
@@ -105,153 +104,78 @@ const MelhoriaGeralSaude = () => {
             <img src={ODS3} alt="" />
           </section>
           <section className={styles.carrossel}>
-
-
-          <a href={imageLinks[currentImage]} target="_blank" rel="noopener noreferrer">
-          <img src={images[currentImage]} alt="" />
-          </a>
-
-        <button className={styles.botao1} onClick={nextImage}> next </button>
-      </section>
+            <a
+              href={imageLinks[currentImage]}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={images[currentImage]} alt="" />
+            </a>
+            <button className={styles.botao1} onClick={nextImage}>
+              next
+            </button>
+          </section>
+          <section className={styles.imagensTextoContainer}>
+            <h1>PODCASTS</h1>
+            <section className={styles.imagensTextoGrid}>
+              <a
+                href="https://soundcloud.com/saudebrasil/o-que-o-cigarro-tem-a-ver-com-o-ganho-ou-perda-de-peso"
+                target="_blank"
+                rel="noopener noreferrer"
+                className={styles.imagensTexto1}
+              >
+                <p>O que cigarro tem a ver com ganho ou perda de peso?</p>
+              </a>
+              <a
+                href="https://soundcloud.com/saudebrasil/como-se-beneficiar-da-alimentacao-saudavel"
+                target="_blank"
+                rel="noopener noreferrer"
+                className={styles.imagensTexto2}
+              >
+                <p>Como se beneficiar da alimentação saudável?</p>
+              </a>
+              <a
+                href="https://soundcloud.com/saudebrasil/como-a-pratica-de-atividade-fisica-pode-melhorar-a-disposicao"
+                target="_blank"
+                rel="noopener noreferrer"
+                className={styles.imagensTexto3}
+              >
+                {" "}
+                <p>Como a atividade física pode ajudar na disposição</p>
+              </a>
+              <a
+                href="https://soundcloud.com/saudebrasil/aleitamento-materno-protege-contra-o-cancer-de-mama"
+                target="_blank"
+                rel="noopener noreferrer"
+                className={styles.imagensTexto4}
+              >
+                {" "}
+                <p>Aleitamento materno protege contra o câncer de mama</p>
+              </a>
+              <a
+                href="https://www.boaconsulta.com/blog/aplicativos-saude-bem-estar/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className={styles.imagensTexto5}
+              >
+                {" "}
+                <p>Aplicativos de saúde que você precisa conhecer!</p>
+              </a>
+              <a
+                href="https://soundcloud.com/saudebrasil/como-esta-sua-saude-mental-em-tempos-de-pandemia"
+                target="_blank"
+                rel="noopener noreferrer"
+                className={styles.imagensTexto6}
+              >
+                {" "}
+                <p>
+                  Como está sua saúde mental em tempos de pós - pandemia?
+                </p>
+              </a>
+            </section>
+          </section>
         </div>
       </div>
-      {/* <div className={styles.gridArea}>
-        <div className={styles.bodyPage}>
-          <section className={styles.odsContainer}>
-            <div className={styles.odsImages}>
-              <img className={styles.baby} src={imgAbs} alt="baby"></img>
-              <img className={styles.babies} src={imgRela} alt="babies"></img>
-            </div>
-            <div className={styles.odsText}>
-              <div>
-                <h1>ODS 3.4 DA ONU</h1>
-                <h3>
-                  Até 2030, reduzir em um terço a mortalidade prematura por
-                  doenças não transmissíveis via prevenção e tratamento,
-                  promover a saúde mental e o bem-estar, a saúde do trabalhador
-                  e da trabalhadora, e prevenir o suicídio, alterando
-                  significativamente a tendência de aumento.
-                </h3>
-              </div>
-            </div>
-          </section>
-          <section className={styles.mulherFalando}>
-            <h2>O que é uma doença crônica?</h2>
-            <img src={mulherFalando} alt="" />
-          </section>
-        </div>
-      </div>
-      <section className={styles.NTransmissivelSection}>
-        <div className={styles.gridNTransmissivel}>
-          <div className={styles.bodyNTransmissivel}>
-            <div className={styles.informacoes}>
-              <div className={styles.grafPizza}>
-                <img src={grafpizza} alt="grafico de pizza" />
-                <p>
-                  69% das mortes ocorridas no Brasil em 2016 <br /> foram por
-                  doenças crônicas não <br /> transmissíveis.
-                </p>
-              </div>
-              <p className={styles.informacoesDestaque}>
-                Maior alerta é na faixa de 30 a 69 anos, quando maioria das
-                mortes são consideradas evitáveis
-              </p>
-            </div>
-            <div className={styles.grafico}>
-              <img src={graflinha} alt="grafico de Linha" />
-            </div>
-          </div>
-        </div>
-      </section>
-      <section className={styles.section2}>
-        <section className={styles.gridArea2}>
-          <div className={styles.bodyPage2}>
-            <section className={styles.doencas}>
-              <h2>Doenças</h2>
-              <div className={styles.doencasFlex}>
-                <Reason icone={diabetes} texto="Diabetes" />
-                <Reason
-                  icone={mentais}
-                  texto="Condições mentais e neurológicas"
-                />
-                <Reason icone={cardio} texto="Doenças cardiovasculares" />
-                <Reason icone={respiratorias} texto="Doenças Respiratórias" />
-                <Reason icone={cancer} texto="Câncer" />
-              </div>
-            </section>
-            <section className={styles.doencas}>
-              <h2 className={styles.doencasH1azul}>Fatores de Risco</h2>
-              <div className={styles.doencasFlex}>
-                <Reason icone={alimentacao} texto="Alimentação inadequada" />
-                <Reason icone={alcool} texto="Uso abusivo de álcool" />
-                <Reason icone={poluicao} texto="Poluiçãou do ar" />
-                <Reason icone={tabagismo} texto="Tabagismo" />
-                <Reason icone={sedentarismo} texto="Sedentarismo" />
-              </div>
-            </section>
-          </div>
-        </section>
-      </section>
-      <section className={styles.BemEstarSection}>
-        <div className={styles.gridBemEstar}>
-          <div className={styles.bodyBemEstar}>
-            <div className={styles.headerBemEstar}>
-              <h1 className={styles.headerH1}>O Bem-Estar</h1>
-              <p className={styles.headerP}>
-                Conforme a Organização Mundial da Saúde, a definição que temos
-                sobre saúde é “um estado de completo bem-estar físico, mental e
-                social, e não apenas a ausência de doenças”
-              </p>
-            </div>
-            <div className={styles.bodyBemEstar}>
-              <BemEstarItem cor="azul" nome="Físico">
-                <p>
-                  Se refere a condição, ausência de doenças associada a um bom
-                  metabolismo.
-                </p>
-              </BemEstarItem>
-              <BemEstarItem cor="azul" nome="Econômico">
-                <p>Está relacionado a condições monetárias.</p>
-              </BemEstarItem>
-              <BemEstarItem cor="azul" nome="Social">
-                <p>
-                  Relacionado a qualidade de vida, envolvendo relações, prazer,
-                  liberdade e saúde ambiental.
-                </p>
-              </BemEstarItem>
-              <BemEstarItem cor="azul" nome="Mental">
-                <p>
-                  É o equilíbrio de sentimentos e emoções e o controle sobre
-                  eles.
-                </p>
-              </BemEstarItem>
-            </div>
-          </div>
-        </div>
-      </section>
-      <section className={styles.TecnlogiaSection}>
-        <div className={styles.gridTecnlogia}>
-          <div className={styles.bodyTecnlogia}>
-            <img src={relogio} alt="" />
-            <div className={styles.TecnlogiaItens}>
-              <h1>Superheroína: A Tecnologia!</h1>
-              <ul>
-                <li>+ monitoramento da própria saúde</li>
-                <li>+ alimentação saudável </li>
-                <li>+ exercícios</li>
-                <li>+ registros de condições</li>
-                <li>
-                  {" "}
-                  <span>
-                    {" "}
-                    = Prevenção de doenças e melhoria no tratamento e cuidado!
-                  </span>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </section> */}
     </div>
   );
 };
