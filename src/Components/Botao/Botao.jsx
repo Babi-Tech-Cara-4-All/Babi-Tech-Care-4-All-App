ec37735d6f9ea6b68b33223856f3d7665500eee8
Lúcia Boutti <luboutti19@gmail.com>
2023-11-24 15:46:19 -0300

    🔹embed video pitch

--- src/Components/Botao/Botao.jsx
+++ src/Components/Botao/Botao.jsx
@@ -1,19 +1,37 @@
+import React from "react";
 import styles from "./Botao.module.scss";
 import { useNavigate } from "react-router-dom";
 
 const Botao = ({ children, cor, rota }) => {
   const navigate = useNavigate();
 
-  function navigateTo(route) {
-    window.scrollTo(0, 0);
-    navigate(`${route}`);
+  function navigateTo(route, targetId) {
+    if (cor === "azul") {
+
+      window.open("https://youtu.be/-MCtv16U--Q", "_blank");
+    } else {
+
+      const targetElement = document.getElementById(targetId);
+
+      if (targetElement) {
+        const offset = targetElement.getBoundingClientRect().top + window.pageYOffset;
+
+        window.scrollTo({
+          top: offset,
+          behavior: "smooth",
+        });
+      } else {
+        window.scrollTo(0, 0);
+      }
+
+      navigate(`${route}`);
+    }
   }
 
   return (
     <button
-      onClick={() => navigateTo(rota)}
+      onClick={() => navigateTo("/", "forms")}
       className={cor === "azul" ? styles.azul : styles.laranja}
-      href=""
     >
       {children}
     </button>
