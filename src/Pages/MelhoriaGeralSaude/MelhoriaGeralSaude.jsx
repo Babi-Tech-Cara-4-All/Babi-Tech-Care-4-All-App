9815b820ec7bd860363e6539396b6a1dc8a19dfd
Lúcia Boutti <luboutti19@gmail.com>
2023-11-24 00:38:19 -0300

    👻 small changes!

--- src/Pages/MelhoriaGeralSaude/MelhoriaGeralSaude.jsx
+++ src/Pages/MelhoriaGeralSaude/MelhoriaGeralSaude.jsx
@@ -45,7 +45,7 @@ const MelhoriaGeralSaude = () => {
         <img src={ODS3} alt="Banner"/>
         </div>
 
-        <div className={styles.txtods}>Reforçar a prevenção e o tratamento dos problemas decorrentes do uso de substâncias, incluindo o abuso de drogas entorpecentes e uso nocivo do álcool.</div>
+        <div className={styles.txtods}><p>Reforçar a prevenção e o tratamento dos problemas decorrentes do uso de substâncias, incluindo o abuso de drogas entorpecentes e uso nocivo do álcool.</p></div>
 
 
       <div className={styles.c1}>
@@ -57,34 +57,34 @@ const MelhoriaGeralSaude = () => {
 
       <div className={styles.Containerpodcast1}>
         <div className={styles.i1}>
-          <img src={i1} alt="i1"/>
+          <a href="https://soundcloud.com/saudebrasil/o-que-o-cigarro-tem-a-ver-com-o-ganho-ou-perda-de-peso" target="_blank" ><img src={i1} alt="i1"/></a>
           <p className={styles.txtpPodcast}>O que cigarro tem a ver com ganho ou perda de peso?</p>
         </div>
 
         <div className={styles.i2}>
-          <img src={i2} alt="i2"/>
+          <a href="https://soundcloud.com/saudebrasil/como-se-beneficiar-da-alimentacao-saudavel" target="_blank" ><img src={i2} alt="i2"/></a>
           <p className={styles.txtpPodcast}>Como se beneficiar da alimentação saudável?</p>
         </div>
 
         <div className={styles.i3}>
-          <img src={i3} alt="i3"/>
+          <a href="https://soundcloud.com/saudebrasil/como-a-pratica-de-atividade-fisica-pode-melhorar-a-disposicao" target="_blank"><img src={i3} alt="i3"/></a>
           <p className={styles.txtpPodcast}>Como a atividade física pode ajudar na disposição</p>
         </div>
       </div>
 
       <div className={styles.Containerpodcast2}>
         <div className={styles.i4}>
-          <img src={i4} alt="i4"/>
+          <a href="https://soundcloud.com/saudebrasil/aleitamento-materno-protege-contra-o-cancer-de-mama" target="_blank"><img src={i4} alt="i4"/></a>
           <p className={styles.txtpPodcast}>Aleitamento materno protege contra o câncer de mama</p>
         </div>
 
         <div className={styles.i5}>
-          <img src={i5} alt="i5"/>
+          <a href="https://www.boaconsulta.com/blog/aplicativos-saude-bem-estar/" target="_blank"><img src={i5} alt="i5"/></a>
           <p className={styles.txtpPodcast}>Aplicativos de saúde que você precisa conhecer!</p>
         </div>
 
         <div className={styles.i6}>
-          <img src={i6} alt="i6"/>
+          <a href="https://soundcloud.com/saudebrasil/como-esta-sua-saude-mental-em-tempos-de-pandemia" target="_blank"><img src={i6} alt="i6"/></a>
           <p className={styles.txtpPodcast}>Como está sua saúde mental em tempos de pós - pandemia?</p>
         </div>
       </div>
@@ -100,9 +100,9 @@ const MelhoriaGeralSaude = () => {
 
         <div className={styles.quadrocinza}>
           
-          <div className={styles.quadroinfo1}>O King’s Fund descreve esse modo de pensar sobre a saúde <br /> da população como um “sistema de saúde da população”, no <br />qual os quatro pilares estão interconectados e a ação é coordenada entre eles, e não dentro de cada um, <br /> isoladamente.</div>
+          <div className={styles.quadroinfo1}> <p>O King’s Fund descreve esse modo de pensar sobre a saúde da população como um “sistema de saúde da população”, no qual os quatro pilares estão interconectados e a ação é coordenada entre eles, e não dentro de cada um, isoladamente.</p></div>
 
-          <div className={styles.quadroinfo2}>Melhorar a saúde da população requer ação em todos os quatro pilares dela e, crucialmente, nas interfaces e sobreposições entre eles.</div>
+          <div className={styles.quadroinfo2}><p>Melhorar a saúde da população requer ação em todos os quatro pilares dela e, crucialmente, nas interfaces e sobreposições entre eles.</p></div>
 
         </div>
 
